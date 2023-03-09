0fe8e6375d9638a9ed8f9b1679460649bffb1882
Tung <hztung3004@gmail.com>
2023-03-09 10:48:02 +0700

    up

--- Lab1/app.js
+++ Lab1/app.js
@@ -1,5 +1,12 @@
-let a = prompt("Moi nhap a:");
-let a1 = parseInt("a");
-
-let b = prompt("moi nhap so:");
-let b1 = parseInt("b");
+//Nhập a
+let a = prompt("Mời nhập a:");
+//Chuyển đổi giá trị nhập vào thành kiểu int
+let a1 = parseInt(a);
+//Nhập b
+let b = prompt("Mời nhập b:");
+//Chuyển đổi giá trị nhập vào thành kiểu int
+let b1 = parseInt(b);
+//Tính tổng
+let c = a1 + b1;
+//in ra
+document.write(c);
