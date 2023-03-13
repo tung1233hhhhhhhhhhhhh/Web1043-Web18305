e2a803bd3b1f53253ac50a96e8ec7634407732fe
Tung <hztung3004@gmail.com>
2023-03-13 13:18:14 +0700

    up

--- Lab2/bai3.js
+++ Lab2/bai3.js
@@ -2,37 +2,27 @@ let fullName = prompt("Nhap ho va ten:");
 let gioiTinh = prompt("Nhap gioi tinh:");
 
 const fullName_list = fullName.split(" ");
+document.writeln("Họ là : " + fullName_list[0].toLocaleUpperCase());
+document.write("<br>");
+document.writeln("Tên đệm là : " + fullName_list[1].toLocaleUpperCase());
+document.write("<br>");
+document.writeln("Tên là : " + fullName_list[2].toLocaleUpperCase());
+document.write("<br>");
+document.write("Giới tính: " + gioiTinh.toLocaleUpperCase());
 if (gioiTinh.toLocaleLowerCase() === "nam") {
-  document.writeln("Họ là : " + fullName_list[0].toLocaleUpperCase());
   document.write("<br>");
-  document.writeln("Tên đệm là : " + fullName_list[1].toLocaleUpperCase());
-  document.write("<br>");
-  document.writeln("Tên là : " + fullName_list[2].toLocaleUpperCase());
-  document.write("<br>");
+
   fullName_list[1] = "Văn";
-  document.write("Giới tính: " + gioiTinh.toLocaleUpperCase());
-  document.write("<br>");
-  let newfull = fullName_list.join(" ");
-  document.writeln("Tên thay đổi : " + newfull.toLocaleUpperCase());
+  var newfull = fullName_list.join(" ");
 } else if (
   gioiTinh.toLocaleLowerCase() === "nữ" ||
   gioiTinh.toLocaleLowerCase() === "nu"
 ) {
-  document.writeln("Họ là : " + fullName_list[0].toLocaleUpperCase());
   document.write("<br>");
-  document.writeln("Tên đệm là : " + fullName_list[1].toLocaleUpperCase());
-  document.write("<br>");
-  document.writeln("Tên là : " + fullName_list[2].toLocaleUpperCase());
-  document.write("<br>");
+
   fullName_list[1] = "Vẽ";
-  document.write("Giới tính: " + gioiTinh.toLocaleUpperCase());
-  document.write("<br>");
-  let newfull = fullName_list.join(" ");
-  document.writeln("Tên thay đổi : " + newfull.toLocaleUpperCase());
+  var newfull = fullName_list.join(" ");
 } else {
   document.write("?");
 }
-
-// let newfull = fullName_list.join(" ");
-
-// document.writeln("Tên mới của bạn là: " + newfull);
+document.writeln("Tên thay đổi : " + newfull.toLocaleUpperCase());
