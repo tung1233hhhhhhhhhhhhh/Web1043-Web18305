ffc2cf0ac547135079da9ac51a62c0ecb0e09ce8
Tung <hztung3004@gmail.com>
2023-04-20 14:49:04 +0700

    up

--- asm/js/header.js
+++ asm/js/header.js
@@ -66,3 +66,216 @@ links.forEach((link) => {
     });
   });
 });
+
+const btnHovers = document.querySelectorAll(".btn-hover");
+
+btnHovers.forEach((btnHover) => {
+  btnHover.addEventListener("mouseover", () => {
+    btnHover.style.boxShadow = "0 0 10px #ccc";
+    btnHover.style.backgroundColor = "#f9f9f9";
+  });
+  btnHover.addEventListener("mouseout", () => {
+    btnHover.style.boxShadow = "none";
+    btnHover.style.backgroundColor = "none";
+  });
+});
+
+//
+
+const iplogin1 = document.querySelector(".login-input-1");
+const lblogin1 = document.querySelector(".login-label-1");
+const iplogin2 = document.querySelector(".login-input-2");
+const lblogin2 = document.querySelector(".login-label-2");
+
+iplogin1.addEventListener("input", function () {
+  const email = iplogin1.value.trim();
+  if (email === "") {
+    iplogin1.style.border = "1px solid blue";
+    lblogin1.style.border = "1px solid blue";
+  } else if (!isValidEmail(email)) {
+    iplogin1.style.border = "1px solid red";
+    lblogin1.style.border = "1px solid red";
+  } else {
+    iplogin1.style.border = "1px solid green";
+    lblogin1.style.border = "1px solid green";
+  }
+});
+
+iplogin1.addEventListener("focus", function () {
+  iplogin1.style.border = "1px solid blue";
+  lblogin1.style.border = "1px solid blue";
+});
+
+iplogin1.addEventListener("blur", function () {
+  const email = iplogin1.value.trim();
+
+  if (email === "") {
+    iplogin1.style.border = "1px solid #ccc";
+    lblogin1.style.border = "none";
+  }
+});
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+//
+
+iplogin2.addEventListener("input", function () {
+  const password = iplogin2.value.trim();
+  if (password.length === 0) {
+    iplogin2.style.border = "1px solid blue";
+    lblogin2.style.border = "1px solid blue";
+  } else if (password.length < 6) {
+    iplogin2.style.border = "1px solid red";
+    lblogin2.style.border = "1px solid red";
+  } else {
+    iplogin2.style.border = "1px solid green";
+    lblogin2.style.border = "1px solid green";
+  }
+});
+
+iplogin2.addEventListener("focus", function () {
+  iplogin2.style.border = "1px solid blue";
+  lblogin2.style.border = "1px solid blue";
+});
+
+iplogin2.addEventListener("blur", function () {
+  const password = iplogin2.value.trim();
+
+  if (password === "") {
+    iplogin2.style.border = "1px solid #ccc";
+    lblogin2.style.border = "none";
+  }
+});
+
+//
+
+const ipsigup1 = document.querySelector(".sigup-input-1");
+const lbsigup1 = document.querySelector(".sigup-label-1");
+const ipsigup2 = document.querySelector(".sigup-input-2");
+const lbsigup2 = document.querySelector(".sigup-label-2");
+const ipsigup3 = document.querySelector(".sigup-input-3");
+const lbsigup3 = document.querySelector(".sigup-label-3");
+const ipsigup4 = document.querySelector(".sigup-input-4");
+const lbsigup4 = document.querySelector(".sigup-label-4");
+
+ipsigup1.addEventListener("input", function () {
+  const username = ipsigup1.value.trim();
+  if (username.length === 0) {
+    ipsigup1.style.border = "1px solid blue";
+    lbsigup1.style.border = "1px solid blue";
+  } else if (username.length < 10) {
+    ipsigup1.style.border = "1px solid red";
+    lbsigup1.style.border = "1px solid red";
+  } else {
+    ipsigup1.style.border = "1px solid green";
+    lbsigup1.style.border = "1px solid green";
+  }
+});
+
+ipsigup1.addEventListener("focus", function () {
+  ipsigup1.style.border = "1px solid blue";
+  lbsigup1.style.border = "1px solid blue";
+});
+
+ipsigup1.addEventListener("blur", function () {
+  const username = ipsigup1.value.trim();
+
+  if (username === "") {
+    ipsigup1.style.border = "1px solid #ccc";
+    lbsigup1.style.border = "none";
+  }
+});
+
+//
+
+ipsigup2.addEventListener("input", function () {
+  const email = ipsigup2.value.trim();
+  if (email === "") {
+    ipsigup2.style.border = "1px solid blue";
+    lbsigup2.style.border = "1px solid blue";
+  } else if (!isValidEmail(email)) {
+    ipsigup2.style.border = "1px solid red";
+    lbsigup2.style.border = "1px solid red";
+  } else {
+    ipsigup2.style.border = "1px solid green";
+    lbsigup2.style.border = "1px solid green";
+  }
+});
+
+ipsigup2.addEventListener("focus", function () {
+  ipsigup2.style.border = "1px solid blue";
+  lbsigup2.style.border = "1px solid blue";
+});
+
+ipsigup2.addEventListener("blur", function () {
+  const email = ipsigup2.value.trim();
+
+  if (email === "") {
+    ipsigup2.style.border = "1px solid #ccc";
+    lbsigup2.style.border = "none";
+  }
+});
+
+//
+
+ipsigup3.addEventListener("input", function () {
+  const password = ipsigup3.value.trim();
+  if (password.length === 0) {
+    ipsigup3.style.border = "1px solid blue";
+    lbsigup3.style.border = "1px solid blue";
+  } else if (password.length < 6) {
+    ipsigup3.style.border = "1px solid red";
+    lbsigup3.style.border = "1px solid red";
+  } else {
+    ipsigup3.style.border = "1px solid green";
+    lbsigup3.style.border = "1px solid green";
+  }
+});
+
+ipsigup3.addEventListener("focus", function () {
+  ipsigup3.style.border = "1px solid blue";
+  lbsigup3.style.border = "1px solid blue";
+});
+
+ipsigup3.addEventListener("blur", function () {
+  const password = ipsigup3.value.trim();
+
+  if (password === "") {
+    ipsigup3.style.border = "1px solid #ccc";
+    lbsigup3.style.border = "none";
+  }
+});
+
+//
+
+ipsigup4.addEventListener("input", function () {
+  const password = ipsigup3.value.trim();
+  const passwordconfirm = ipsigup4.value.trim();
+  if (passwordconfirm.length === 0) {
+    ipsigup4.style.border = "1px solid blue";
+    lbsigup4.style.border = "1px solid blue";
+  } else if (passwordconfirm != password || passwordconfirm.length < 6) {
+    ipsigup4.style.border = "1px solid red";
+    lbsigup4.style.border = "1px solid red";
+  } else {
+    ipsigup4.style.border = "1px solid green";
+    lbsigup4.style.border = "1px solid green";
+  }
+});
+
+ipsigup4.addEventListener("focus", function () {
+  ipsigup4.style.border = "1px solid blue";
+  lbsigup4.style.border = "1px solid blue";
+});
+
+ipsigup4.addEventListener("blur", function () {
+  const passwordconfirm = ipsigup4.value.trim();
+
+  if (passwordconfirm === "") {
+    ipsigup4.style.border = "1px solid #ccc";
+    lbsigup4.style.border = "none";
+  }
+});
